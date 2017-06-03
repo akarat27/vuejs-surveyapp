d73125dd3371336c518c9c1b76bd4bc3a54bd32f
Akarat Suvitayasiri <akrs33@gmail.com>
2017-06-03 10:06:44 +0700

    Startup simple vuejs pwa

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <Modal
+    v-model="modal1"
+    title=""
+    @on-ok="ok"
+    @on-cancel="cancel">
+    <p slot="header">555</p>
+    <p>Login facebook</p>
+  </Modal>
+</template>
+<script>
+    export default {
+      data () {
+        return {
+          modal1: true
+        }
+      },
+      methods: {
+        ok () {
+          // this.$Message.info('点击了确定');
+          console.log('ok')
+        },
+        cancel () {
+          // this.$Message.info('点击了取消');
+          console.log('cancel')
+        }
+      }
+    }
+</script>
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/hello',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '',
+      name: 'Login',
+      component: Login
+    }
+  ],
+  mode: 'history'
+})
